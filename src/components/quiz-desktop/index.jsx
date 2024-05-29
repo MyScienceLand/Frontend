1a19e6f1b62de71830ac49645caaf590e0a1f6b9
billydev139 <bilalbhatti139@gmail.com>
2024-05-29 20:51:08 +0500

    course content

--- src/components/quiz-desktop/index.jsx
+++ src/components/quiz-desktop/index.jsx
@@ -4,21 +4,39 @@ const QuizDesktop = () => {
   const sectionData = [
     {
       course: "A Levels",
-      class: "bg-primary px-4 py-2 text-[16px] font-normal",
-      style: "bg-[#006C8D] px-4 py-1 text-[16px] font-normal",
+      className:
+        "bg-primary px-6 py-2 text-[16px] text-white font-normal cursor-pointer",
+      style:
+        "bg-[#006C8D] text-white px-4 py-1 text-[16px] font-normal cursor-pointer",
+      subject: "biology",
+    },
+    {
+      course: "A Levels",
+      className:
+        "bg-primary px-6 py-2 text-[16px] text-white font-normal cursor-pointer",
+      style:
+        "bg-[#006C8D] text-white px-4 py-1 text-[16px] font-normal cursor-pointer",
+      subject: "biology",
+    },
+    {
+      course: "A Levels",
+      className:
+        "bg-primary px-6 py-2 text-[16px] text-white font-normal cursor-pointer",
+      style:
+        "bg-[#006C8D] text-white px-4 py-1 text-[16px] font-normal cursor-pointer",
       subject: "biology",
     },
   ];
   return (
-    <div className="container mx-auto">
+    <div className="">
       {sectionData.map((item, index) => (
-        <div className="flex gap-6 justify-center items-center">
+        <div className="flex gap-6 justify-center mb-4 items-center">
           <div className={item.className}>{item.course}</div>
           <div className={item.style}>{item.subject}</div>
-          <div className="text-[16px] px-20 py-2 font-normal border border-[#696969]">
+          <div className="text-[16px] px-40 py-2 font-normal border border-[#696969]">
             Paper 1
           </div>
-          <div className="text-[16px] px-20 py-2 font-normal border rounded border-[#696969]">
+          <div className="text-[16px] px-40 py-2 font-normal border  rounded border-[#696969]">
             Paper 2
           </div>
         </div>
