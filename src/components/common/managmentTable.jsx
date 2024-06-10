e213fe6fe223a2346e91c7fff5e718bb5430d7fa
billydev139 <bilalbhatti139@gmail.com>
2024-06-10 20:06:08 +0500

    managment table

--- src/components/common/managmentTable.jsx
+++ src/components/common/managmentTable.jsx
@@ -38,77 +38,70 @@ const brandData = [
 
 const ManagmentTable = () => {
   return (
-    <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Top Channels
-      </h4>
+    <>
+      <div className="rounded-lg shadow-sm bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+        <h4 className="mb-6 text-primary text-xl font-semibold">
+          Top Channels
+        </h4>
 
-      <div className="flex flex-col">
-        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
-          <div className="p-2.5 xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Source
-            </h5>
+        <div className="flex flex-col">
+          <div className="grid grid-cols-3 bg-[#F0E8FF] rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+            <div className="p-2.5 text-center xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Topic
+              </h5>
+            </div>
+            <div className="p-2.5 text-center xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Topic Name
+              </h5>
+            </div>
+            <div className="p-2.5 text-center xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Percentage
+              </h5>
+            </div>
+            <div className="hidden p-2.5 text-center sm:block xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Performance
+              </h5>
+            </div>
           </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Visitors
-            </h5>
-          </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Revenues
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Sales
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Conversion
-            </h5>
-          </div>
-        </div>
 
-        {brandData.map((brand, key) => (
-          <div
-            className={`grid grid-cols-3 sm:grid-cols-5 ${
-              key === brandData.length - 1
-                ? ""
-                : "border-b border-stroke dark:border-strokedark"
-            }`}
-            key={key}
-          >
-            <div className="flex items-center gap-3 p-2.5 xl:p-5">
-              <div className="flex-shrink-0">
-                <img src={brand.logo} alt="Brand" />
+          {brandData.map((brand, key) => (
+            <div
+              className={`grid grid-cols-3 sm:grid-cols-5 ${
+                key === brandData.length - 1
+                  ? ""
+                  : "border-b border-stroke dark:border-strokedark"
+              }`}
+              key={key}
+            >
+              <div className="flex items-center gap-3 p-2.5 xl:p-5">
+                <div className="flex items-start justify-center p-2.5 xl:p-5">
+                  <p className="text-black ">{brand.topic}</p>
+                </div>
+                <p className="hidden text-black  sm:block">{brand.name}</p>
               </div>
-              <p className="hidden text-black dark:text-white sm:block">
-                {brand.name}
-              </p>
-            </div>
 
-            <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-black dark:text-white">{brand.visitors}K</p>
-            </div>
+              <div className="flex items-center justify-center p-2.5 xl:p-5">
+                <p className="text-black ">{brand.visitors}K</p>
+              </div>
 
-            <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-meta-3">${brand.revenues}</p>
-            </div>
+              <div className="flex items-center justify-center p-2.5 xl:p-5">
+                <p className="text-meta-3">${brand.revenues}</p>
+              </div>
 
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black dark:text-white">{brand.sales}</p>
+              <div className="hidden items-center justify-center  p-2.5 sm:flex xl:p-5">
+                <p className="px-4 py-1 rounded-lg text-[#54B17E] bg-[#DDFAEA] ">
+                  {brand.sales}
+                </p>
+              </div>
             </div>
-
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-meta-5">{brand.conversion}%</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/managment-classes/index.jsx
+++ src/components/managment-classes/index.jsx
@@ -3,8 +3,47 @@ import ManagmentTable from "../common/managmentTable";
 
 const ManagmentClasses = () => {
   return (
-    <div>
-      <ManagmentTable />
+    <div className="grid grid-cols-[1fr_1fr] gap-4">
+      <div>
+        {" "}
+        <div className="flex flex-col  justify-center items-center">
+          <ManagmentTable />
+
+          <button className="bg-primary mt-5 text-white px-6 py-2 ">
+            View All
+          </button>
+        </div>
+      </div>
+      <div>
+        {" "}
+        <div className="flex flex-col  justify-center items-center">
+          <ManagmentTable />
+
+          <button className="bg-primary mt-5 text-white px-6 py-2 ">
+            View All
+          </button>
+        </div>
+      </div>
+      <div>
+        {" "}
+        <div className="flex flex-col  justify-center items-center">
+          <ManagmentTable />
+
+          <button className="bg-primary mt-5 text-white px-6 py-2 ">
+            View All
+          </button>
+        </div>
+      </div>
+      <div>
+        {" "}
+        <div className="flex flex-col  justify-center items-center">
+          <ManagmentTable />
+
+          <button className="bg-primary mt-5 text-white px-6 py-2 ">
+            View All
+          </button>
+        </div>
+      </div>{" "}
     </div>
   );
 };
